bd6152561564fe6434240cda15f406d5d83c340c
Mary Likam <159551176+Doclikam@users.noreply.github.com>
2025-12-10 08:26:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -797,8 +797,8 @@ with tab_patient:
                             surv_plot[["period", "months", "date", "S_control", "S_treat"]].head(),
                             use_container_width=True
                         )
-surv = out.get("survival_curve")
-st.write("DEBUG — surv.head():", surv.head() if surv is not None else "surv is None")
+                    surv = out.get("survival_curve")
+                    st.write("DEBUG — surv.head():", surv.head() if surv is not None else "surv is None")
 
 
             # ---------- PATIENT REPORT SUMMARY ----------
